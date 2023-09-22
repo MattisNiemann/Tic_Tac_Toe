889b02d0fe61b22290f7b5b0b216066f8159ad22
Mattis Niemann <mattisniemann08@gmail.com>
2023-09-22 23:21:12 +0200

    cell value can be canged to x by clicking on them

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-let grid = [[1,2,3][4,5,6][7,8,9]]
+//let grid = [,2,3,4,5,6,7,8,9]
+let playerXTurn = new Boolean(false)
+let grid = [document.getElementById("cell1").innerHTML,document.getElementById("cell2").innerHTML,
+        document.getElementById("cell3").innerHTML,document.getElementById("cell4").innerHTML,
+        document.getElementById("cell5").innerHTML,document.getElementById("cell6").innerHTML,
+        document.getElementById("cell7").innerHTML,document.getElementById("cell8").innerHTML,
+        document.getElementById("cell9").innerHTML
+    ]
+
+
+
+
+//console.log(xc*1)
 console.log(grid)
 
+function setSymbol(a){
+    grid[a] = "x";
+    document.getElementById("cell"+(a+1)).innerHTML = "x";
+    console.log(a+1)
+    console.log(grid)
+    
 
-let x = document.getElementById(x)
-console.log(x)
+
+}
